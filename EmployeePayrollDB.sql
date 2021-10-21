5f510e818a1a993a35e5374bda7c200b499cb7f8
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-21 13:09:28 +0530

    Added a SQL query to create a database table

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -2,3 +2,10 @@
 --UC-1: Creating a database
 create database payroll_service;
 USE payroll_service;
+--UC-2: Creating a table
+create table employee_payroll(
+	Id int identity(1,1) primary key,
+	Name varchar (200),
+	Salary float,
+	StartDate date
+	);
