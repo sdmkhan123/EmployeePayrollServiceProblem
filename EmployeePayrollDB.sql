b6ff48883fdde072b2bc0d2a63627bec4e1f0d8b
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 19:38:48 +0530

    Extend and Rename Salary to Basic Pay Employee Data And Informations

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -40,3 +40,10 @@ phone varchar(13),
 department varchar(100) not null default 'B.E',
 address varchar(250) not null default 'Banglore'
 select * from employee_payroll
+--UC-9
+sp_rename 'employee_payroll.salary' , 'basic_pay'
+alter table employee_payroll add
+deduction float ,
+taxable_pay float,
+incomeTax float,
+net_pay float;
